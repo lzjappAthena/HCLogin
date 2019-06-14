37927d0363e58027cc9328aa75a42b3d8f8c3438
lizhongjun <798956212@qq.com>
2019-06-14 15:19:18 +0800

    Initial commit

---
+++ Example/HCLogin/HCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  HCAppDelegate.h
+//  HCLogin
+//
+//  Created by lzjappAthena on 06/14/2019.
+//  Copyright (c) 2019 lzjappAthena. All rights reserved.
+//
+
+@import UIKit;
+
+@interface HCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
